85c4f39f2a9dd1575a58e5497bfc6dd088da398b
Matty1301 <matty_bates@live.co.uk>
2025-01-25 19:16:55 +0000

    Add hazard behaviour

---
+++ Assets/Scripts/Hazard.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Hazard : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Player player = collision.gameObject.GetComponent<Player>();
+        if (player != null)
+        {
+            player.Damage();
+        }
+    }
+}
